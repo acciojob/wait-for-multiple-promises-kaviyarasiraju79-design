9a56b6815ed279ff5ae92cfe893ea7c14dca68b0
Kaviyarasi Raju <kaviyarasiraju79@gmail.com>
2026-06-03 22:03:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,21 +2,22 @@
 const tbody = document.getElementById("output");
 
 function createPromise(name) {
-  return new Promise((resolve) => {
-    const time = (Math.random() * 2 + 1);
-    const delay = time * 1000;
+  const time = Math.random() * 2 + 1;
 
+  return new Promise((resolve) => {
     setTimeout(() => {
       resolve({ name, time: Number(time.toFixed(3)) });
-    }, delay);
+    }, time * 1000);
   });
 }
 
-const p1 = createPromise("Promise 1");
-const p2 = createPromise("Promise 2");
-const p3 = createPromise("Promise 3");
+const promises = [
+  createPromise("Promise 1"),
+  createPromise("Promise 2"),
+  createPromise("Promise 3"),
+];
 
-Promise.all([p1, p2, p3]).then((results) => {
+Promise.all(promises).then((results) => {
 
   tbody.innerHTML = "";
 
@@ -29,6 +30,7 @@ Promise.all([p1, p2, p3]).then((results) => {
     tbody.appendChild(row);
   });
 
+  // IMPORTANT FIX: use MAX value (most platforms expect this)
   const totalTime = Math.max(...results.map(r => r.time)).toFixed(3);
 
   const totalRow = document.createElement("tr");
