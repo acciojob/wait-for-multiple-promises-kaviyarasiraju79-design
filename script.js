c6bbb177bde0bccbb3971f257489375728ef3742
Kaviyarasi Raju <kaviyarasiraju79@gmail.com>
2026-06-06 00:00:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,35 +2,45 @@
 const tbody = document.getElementById("output");
 
 function createPromise(name) {
-  const time = Math.random() * 2 + 1;
+  const time = Math.random() * 2 + 1; // 1 to 3 seconds
 
   return new Promise((resolve) => {
     setTimeout(() => {
-      resolve({ name, time: Number(time.toFixed(3)) });
+      resolve({
+        name: name,
+        time: time
+      });
     }, time * 1000);
   });
 }
 
-const promises = [
+const startTime = performance.now();
+
+Promise.all([
   createPromise("Promise 1"),
   createPromise("Promise 2"),
-  createPromise("Promise 3"),
-];
-
-const start = Date.now();
-
-Promise.all(promises).then((results) => {
-  const totalTime = ((Date.now() - start) / 1000).toFixed(3);
-
+  createPromise("Promise 3")
+]).then((results) => {
   tbody.innerHTML = "";
 
-  results.forEach((res) => {
+  results.forEach((result) => {
     const row = document.createElement("tr");
-    row.innerHTML = `<td>${res.name}</td><td>${res.time}</td>`;
+
+    row.innerHTML = `
+      <td>${result.name}</td>
+      <td>${result.time.toFixed(3)}</td>
+    `;
+
     tbody.appendChild(row);
   });
 
+  const totalTime = (performance.now() - startTime) / 1000;
+
   const totalRow = document.createElement("tr");
-  totalRow.innerHTML = `<td>Total</td><td>${totalTime}</td>`;
+  totalRow.innerHTML = `
+    <td>Total</td>
+    <td>${totalTime.toFixed(3)}</td>
+  `;
+
   tbody.appendChild(totalRow);
 });
